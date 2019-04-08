4122e8d157aa7d67241b4a4781c3f4e86bc8dd26
Adam Ulbrich <kermit.xc@gmail.com>
2019-04-08 01:11:05 -0500

    adding post management

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm 
 from flask_wtf.file import FileField, FileAllowed
-from wtforms import StringField, PasswordField, SubmitField, BooleanField 
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskblog.models import User
 from flask_login import current_user
@@ -53,3 +53,8 @@ class UpdateAccountForm(FlaskForm):
             user = User.query.filter_by(email=email.data).first()
             if user:
                 raise ValidationError('That username is already taken. Please try again')
+
+class PostForm(FlaskForm): 
+    title = StringField('Title', validators=[DataRequired()])
+    postContent = TextAreaField('Content', validators=[DataRequired()])
+    submit = SubmitField('Post')
