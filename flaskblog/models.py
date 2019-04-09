b8aa65010fa266d9e3696e8cb88e023785054852
Adam Ulbrich <kermit.xc@gmail.com>
2019-04-08 19:29:51 -0500

    circleCI is no more ... maybe again soon, but not today

--- flaskblog/models.py
+++ flaskblog/models.py
@@ -21,7 +21,7 @@ class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    content = db.Column(db.Text, nullable=False)
+    content = db.Column(db.Text, nullable=Falsenone)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     def __repr__(self):
